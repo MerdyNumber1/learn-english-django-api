16c74f645654b3476ea66efc882c1a1bb3eb4179
merdy <sampekmoy@gmail.com>
2022-02-14 00:05:25 +0300

    fix: consumer event for all channels

--- app/chat/consumers.py
+++ app/chat/consumers.py
@@ -1,4 +1,5 @@
 from channels.generic.websocket import JsonWebsocketConsumer
+from asgiref.sync import async_to_sync
 
 from . import services as chat_services
 from .serializers import MessageSerializer
@@ -6,16 +7,24 @@ from .serializers import MessageSerializer
 
 class ChatConsumer(JsonWebsocketConsumer):
     def connect(self):
+        async_to_sync(self.channel_layer.group_add)("chat", self.channel_name)
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)("chat", self.channel_name)
 
     def receive_json(self, content):
         serializer = MessageSerializer(data=content)
 
         if serializer.is_valid():
-            message = chat_services.create_message(content, self.scope['user'])
-            self.send_json(content=MessageSerializer(message).data)
+            async_to_sync(self.channel_layer.group_send)(
+                "chat",
+                {
+                    "type": "chat.message",
+                    "text": content,
+                },
+            )
 
-
+    def chat_message(self, event):
+        message = chat_services.create_message(event['text'], self.scope['user'])
+        self.send_json(content=MessageSerializer(message).data)
